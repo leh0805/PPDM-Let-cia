ebdc16d40682c874be991bb2e967e0489658a67b
Letícia <100808649+leh0805@users.noreply.github.com>
2023-10-19 10:48:18 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Este é meu primeiro App da vida toda :D </Text>
+      <Text>happy happy happy</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
